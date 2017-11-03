5d9291f34fad6d54b596c0d775a0214bfcca1aa5
gabrielx52 <gabriel.meringolo@gmail.com>
2017-11-02 17:31:37 -0700

    Added jinja templating.

--- learning_journal/routes.py
+++ learning_journal/routes.py
@@ -3,7 +3,7 @@
 
 def includeme(config):
     """Route for pyramid learning journal."""
-    config.add_static_view('static', 'static', cache_max_age=3600)
+    config.add_static_view('static', 'static', cache_max_age=30)
     config.add_route('home', '/')
     config.add_route('post', '/journal/{id:\d+}')
     config.add_route('new-entry', '/journal/new-entry')
--- learning_journal/views/default.py
+++ learning_journal/views/default.py
@@ -1,21 +1,25 @@
 """View serve functions."""
-from pyramid.view import view_config
 import os
 
-HERE = os.path.abspath(__file__)
-TEMPLATE = os.path.join(os.path.dirname(os.path.dirname(HERE)), 'templates')
+from learning_journal.data.entries import ENTRIES
+
+from pyramid.view import view_config
 
 
 @view_config(route_name="home", renderer="templates/index.jinja2")
 def list_view(request):
     """Serve the main learning journal page."""
-    return{}
+    return{"entries": ENTRIES}
 
 
 @view_config(route_name="post", renderer="templates/detail.jinja2")
 def detail_view(request):
     """Serve detail page for single entry."""
-    return{}
+    post_id = int(request.matchdict['id'])
+    # import pdb; pdb.set_trace()
+    for entry in ENTRIES:
+        if entry['id'] == post_id:
+            return{'entry': entry}
 
 
 @view_config(route_name="new-entry", renderer="templates/new_entry.jinja2")
