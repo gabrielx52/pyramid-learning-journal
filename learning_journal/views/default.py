a9d1b13b8b4d3b667dfa57b68fd17dd858c2872a
gabrielx52 <gabriel.meringolo@gmail.com>
2017-11-03 15:47:20 -0700

    Integrated postgres database. Made LJ Entry model, added database urls, updated initdb to create table and load data from data dir.

--- learning_journal/views/default.py
+++ learning_journal/views/default.py
@@ -1,25 +1,30 @@
 """View serve functions."""
-import os
-
 from learning_journal.data.entries import ENTRIES
-
+from learning_journal.models.mymodel import Entry
 from pyramid.view import view_config
 
 
 @view_config(route_name="home", renderer="templates/index.jinja2")
 def list_view(request):
     """Serve the main learning journal page."""
-    return{"entries": ENTRIES}
+    entries = request.dbsession.query(Entry).all()
+    entries = [entry.to_dict() for entry in entries]
+    return {
+        "entries": entries
+    }
 
 
 @view_config(route_name="post", renderer="templates/detail.jinja2")
 def detail_view(request):
     """Serve detail page for single entry."""
     post_id = int(request.matchdict['id'])
-    # import pdb; pdb.set_trace()
-    for entry in ENTRIES:
-        if entry['id'] == post_id:
-            return{'entry': entry}
+    # for entry in ENTRIES:
+    #     if entry['id'] == post_id:
+    #         return{'entry': entry}
+    entry = request.dbsession.query(Entry).get(post_id)
+    return {
+        'entry': entry.to_dict()
+    }
 
 
 @view_config(route_name="new-entry", renderer="templates/new_entry.jinja2")
