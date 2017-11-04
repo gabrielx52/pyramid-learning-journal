d7824c421e7ab00a52d697e5c343d2a8b8367775
gabrielx52 <gabriel.meringolo@gmail.com>
2017-11-03 23:21:38 -0700

    New and update views work. Can add new and update old entries.

--- learning_journal/views/default.py
+++ learning_journal/views/default.py
@@ -1,6 +1,8 @@
 """View serve functions."""
+from datetime import datetime
+
 from pyramid.view import view_config
-from pyramid.httpexceptions import HTTPNotFound
+from pyramid.httpexceptions import HTTPNotFound, HTTPBadRequest, HTTPFound
 from learning_journal.models.mymodel import Entry
 
 
@@ -29,10 +31,35 @@ def detail_view(request):
 @view_config(route_name="new-entry", renderer="templates/new_entry.jinja2")
 def create_view(request):
     """Serve the create a new entry page."""
-    return{}
+    if request.method == "GET":
+        return{}
+    if request.method == "POST":
+        if not all([field in request.POST for field in ['title', 'body']]):
+            raise HTTPBadRequest
+        new_entry = Entry(
+            title=request.POST['title'],
+            body=request.POST['body'],
+            creation_date=datetime.now()
+        )
+        request.dbsession.add(new_entry)
+        return HTTPFound(request.route_url('home'))
 
 
 @view_config(route_name="edit-entry", renderer="templates/edit_entry.jinja2")
 def update_view(request):
     """Serve the edit an entry page."""
-    return{}
+    entry_id = int(request.matchdict['id'])
+    entry = request.dbsession.query(Entry).get(entry_id)
+    if not entry:
+        raise HTTPNotFound
+    if request.method == "GET":
+        return {
+            'entry': entry.to_dict()
+        }
+    if request.method == "POST":
+        entry.title = request.POST['title']
+        entry.body = request.POST['body']
+        entry.creation_date = datetime.now()
+        request.dbsession.add(entry)
+        request.dbsession.flush()
+        return HTTPFound(request.route_url('post', id=entry.id))
